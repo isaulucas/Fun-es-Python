19d505e819f07a76e1408ead07f5bbb3e0a7d542
Isaú Lucas de Queiroz <84081130+isaulucas@users.noreply.github.com>
2022-08-18 11:46:02 -0300

    Create questao10.py
    Exercício 10: Escreva uma função para contar as ocorrências de todos os caracteres dentro de uma 
    string

---
+++ questao10.py
@@ -0,0 +1,12 @@
+# função count 
+
+if __name__ == '__main__':
+ 
+    string = "Anna e Anne"
+ 
+    oc = string.count('A')
+    oc2 = string.count('n')
+    oc3 = string.count('e')
+   
+                                
+print ('A =',oc ,'n =',oc2 ,'e =', oc3)
