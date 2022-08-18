4edf7c6220d1db3ae2d2305b7dc9cdf588dc78b5
Liza Magalhães <84081056+lizamagalhaes@users.noreply.github.com>
2022-08-18 20:03:51 -0300

    Create questao19.py

---
+++ questao19.py
@@ -0,0 +1,6 @@
+#Exercício 19: Escreva um programa para remover o item presente no índice 4 e adicioná-lo na 2ª posição e no final da lista.
+
+print("Digite pelo menos 6 palavras para criar uma lista:")
+
+lista = [input()]
+print(lista)
