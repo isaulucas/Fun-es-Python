edf1cc9ba29cc26c8712a9bbd01784a37f11101d
Liza Magalhães <84081056+lizamagalhaes@users.noreply.github.com>
2022-08-17 21:35:11 -0300

    Update questao13.py

--- questao13.py
+++ questao13.py
@@ -1,5 +1,20 @@
-print("Digite uma frase:")
-frase = input().split()
+print("digite uma frase:")
+
+def separar_frase(palavras):
+    lista_de_palavras = palavras.split(' ')
+    return lista_de_palavras
+
+def juntar_palavras(lista_de_palavras):
+    palavras = '-'.join(lista_de_palavras)
+    return palavras
+
+if __name__ == '__main__':
+    palavras = input()
+
+    frase = separar_frase(palavras)
+
+    nova_frase = juntar_palavras(frase)
+    print(nova_frase)
 
 for palavras in frase:
     print(palavras)
