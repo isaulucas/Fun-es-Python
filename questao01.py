7a55da466a1c4996ebb50d785417bca9930521ae
Liza Magalhães <84081056+lizamagalhaes@users.noreply.github.com>
2022-08-18 22:26:31 -0300

    Update questao01.py

--- questao01.py
+++ questao01.py
@@ -6,4 +6,4 @@ print("esta eh sua palavra:", palavra)
 tamanho_da_palavra = len(palavra)
 metade_da_palavra = (tamanho_da_palavra//2)
 nova_palavra = palavra[0] + palavra[metade_da_palavra] + palavra[-1]
-print("esta eh sua palavra apenas com a primeira letra, a letra do meio e a letra final:", nova_palavra)
+print("\nEsta eh sua palavra apenas com a primeira letra, a letra do meio e a letra final:", nova_palavra)
