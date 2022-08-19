3c353317eab5d6b7587d052a3cc082e52fecb0f4
Liza Magalhães <84081056+lizamagalhaes@users.noreply.github.com>
2022-08-18 21:06:14 -0300

    Update questao19.py

--- questao19.py
+++ questao19.py
@@ -1,6 +1,11 @@
 #Exercício 19: Escreva um programa para remover o item presente no índice 4 e adicioná-lo na 2ª posição e no final da lista.
 
-print("Digite pelo menos 6 palavras para criar uma lista:")
-
-lista = [input()]
-print(lista)
+print("Vamos criar uma lista?")
+lista = []
+lista.append(input("Insira a palavra 1:"))
+lista.append(input("Insira a palavra 2:"))
+lista.append(input("Insira a palavra 3:"))
+lista.append(input("Insira a palavra 4:"))
+lista.append(input("Insira a palavra 5:"))
+lista.append(input("Insira a palavra 6:"))
+print("Essa eh sua lista: ", lista)
